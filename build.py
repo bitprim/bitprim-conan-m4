4eeb6a4ecac934e8ec468efa1e6e81df7c76ff0a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-09-28 15:12:16 -0300

    just release builds

--- build.py
+++ build.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     # builder.add_common_builds(shared_option_name="m4:shared")
     builder.add_common_builds()
 
-    # filtered_builds = []
-    # for settings, options, env_vars, build_requires in builder.builds:
-    #     if settings["build_type"] == "Release":
-    #         filtered_builds.append([settings, options, env_vars, build_requires])
-    # builder.builds = filtered_builds
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["build_type"] == "Release":
+            filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
 
 
     builder.run()
